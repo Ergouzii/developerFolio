3cec321e4dff44f7361b15f3c55e326c119a694a
Ergouzii <jayliu925@gmail.com>
2020-09-27 19:04:26 +0000

    Deploying to gh-pages from  @ 90ca3e7dedaa0980ebbe700c3c1cdd024424d863 🚀

---
+++ precache-manifest.bebe1aca9bc516844220f3168adbdeee.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acf7dcdaad0efc97c27c36d3d7f4c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14116212e194ed16ad5f",
+    "url": "/static/css/3.8a279449.chunk.css"
+  },
+  {
+    "revision": "dad14cb93d0b80906e7a",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "1d7f735160d3a488cc55",
+    "url": "/static/css/main.84bab017.chunk.css"
+  },
+  {
+    "revision": "ccf3f7a0fa7090556630",
+    "url": "/static/js/2.d985951f.chunk.js"
+  },
+  {
+    "revision": "14116212e194ed16ad5f",
+    "url": "/static/js/3.bb2c227f.chunk.js"
+  },
+  {
+    "revision": "dad14cb93d0b80906e7a",
+    "url": "/static/js/4.13f9723a.chunk.js"
+  },
+  {
+    "revision": "1d7f735160d3a488cc55",
+    "url": "/static/js/main.d8d80c11.chunk.js"
+  },
+  {
+    "revision": "5f830f5c9d3ae474b674",
+    "url": "/static/js/runtime-main.b7f233ae.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dcf4eb25e8b7cc503296f7b6aa752096",
+    "url": "/static/media/aws_logo.svg.dcf4eb25.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "48a892e5d76bf282c5bfc825797af9e6",
+    "url": "/static/media/fcc_logo.48a892e5.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/static/media/nodejs_logo.svg.270f55ff.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "b5d90fb24f45472ea9074d28da9568da",
+    "url": "/static/media/ualberta_logo.b5d90fb2.png"
+  }
+]);
