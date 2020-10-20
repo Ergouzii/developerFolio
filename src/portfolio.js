64b0db8e4af8c739ec95316248bf05536d9a0910
xiaohui-j-liu <xiaohui.j.liu@gmail.com>
2020-10-20 17:44:59 -0600

    Update resume link

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const greeting = {
     'A passionate Software Developer 🚀 who loves building web applications with React.js / Nodejs / Django and some other cool libraries and frameworks.'
   ),
   resumeLink:
-    'https://docs.google.com/document/d/13fL4IivGrph-55tWBT6soskozCSgFeqSehdf6drKYFk/preview',
+    'https://docs.google.com/document/d/13fL4IivGrph-55tWBT6soskozCSgFeqSehdf6drKYFk/export?format=pdf&attachment=false',
 };
 
 // Social Media Link
